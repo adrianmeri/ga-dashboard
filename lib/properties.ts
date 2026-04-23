8f1339a7d933925ba5a5018888fed108445ae61a
adrianmeri <adrian.meri@gmail.com>
2026-04-23 22:50:24 +0200

    Add Spojena Bratislava property

--- lib/properties.ts
+++ lib/properties.ts
@@ -2,6 +2,7 @@ export const properties = [
   { id: '419643819', name: 'Downtown Yards', colors: ['rgb(1,88,73)', 'rgb(1,120,100)', 'rgb(1,150,125)', 'rgb(1,180,150)'] },
   { id: '400697744', name: 'Ganz House',     colors: ['rgb(160,130,100)', 'rgb(180,155,125)', 'rgb(200,178,150)', 'rgb(220,200,175)'] },
   { id: '250756827', name: 'Bratislava Offices', colors: ['#6366f1', '#8b5cf6', '#a78bfa', '#c4b5fd'] },
+  { id: '399935530', name: 'Spojena Bratislava',  colors: ['rgb(131,205,190)', 'rgb(155,215,205)', 'rgb(180,225,215)', 'rgb(205,235,230)'] },
 ];
 
 export const defaultPropertyId = properties[0].id;
